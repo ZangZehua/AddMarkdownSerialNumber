f6e2cebd6ac50c5d2fc37765ae6b98899f99a8e2
ZangZehua <zhzang97@gmail.com>
2021-12-22 20:23:24 +0800

    complete simple project and add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Add Serial Number to Markdown file
+
+```angular2html
+python add.py -i [path of original markdown file] 
+              -o [path of target markdown file(with serial number) 
+              -m [0: end with blank space; 1: ent with .]]
+```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 
 def get_common_args():
@@ -11,34 +12,83 @@ def get_common_args():
     return args
 
 
-def generate_serial_number(serial_number_list, mode):
+def generate_serial_number(serial_number_list, level, mode):
     serial_number = ""
     if mode == 0:
-        for sn in serial_number_list[:-1]:
+        for sn in serial_number_list[:level-1]:
             serial_number += str(sn) + "."
-        serial_number += str(serial_number_list[-1])
+        serial_number += str(serial_number_list[level-1])
+        serial_number += " "
     elif mode == 1:
-        for sn in serial_number_list:
+        for sn in serial_number_list[:level]:
             serial_number += str(sn) + "."
     return serial_number
 
-def add_serial_number(input_path, output_path):
+
+def add_serial_number(args):
     try:
-        output_file = open(output_path, "w")
+        output_file = open(args.o, "w", encoding='UTF-8')
+        print("successfully open output file")
     except Exception as e:
-        print("fail to open file" + output_path)
+        print("fail to open file" + args.o)
+        exit()
 
-    with open(input_path, "r") as input_file:
-        serial_number_list = [1]*6
+    with open(args.i, "r", encoding='UTF-8') as input_file:
+        serial_number_list = [0]*5
         temp_line = input_file.readline()
-
-
+        counter = 0
+        # in_code = False
+        while temp_line is not None and counter < 1000:
+            # if re.match("```", temp_line) is not None:
+            #     temp_line = input_file.readline()
+            #     counter += 1
+            #     in_code = not in_code
+            #     print(in_code)
+            # elif in_code:
+            #     temp_line = input_file.readline()
+            #     counter += 1
+            #     print("still in code")
+            #     continue
+            if re.match("###### ", temp_line) is not None:
+                serial_number_list[4] += 1
+                serial_number = generate_serial_number(serial_number_list, 5, args.m)
+                temp_line = temp_line.replace("###### ", "###### " + str(serial_number), 1)
+                output_file.write(temp_line)
+            elif re.match("##### ", temp_line) is not None:
+                serial_number_list[3] += 1
+                serial_number_list[4:5] = [0]
+                serial_number = generate_serial_number(serial_number_list, 4, args.m)
+                temp_line = temp_line.replace("##### ", "##### " + str(serial_number), 1)
+                output_file.write(temp_line)
+            elif re.match("#### ", temp_line) is not None:
+                serial_number_list[2] += 1
+                serial_number_list[3:5] = [0, 0]
+                serial_number = generate_serial_number(serial_number_list, 3, args.m)
+                temp_line = temp_line.replace("#### ", "#### " + str(serial_number), 1)
+                output_file.write(temp_line)
+            elif re.match("### ", temp_line) is not None:
+                serial_number_list[1] += 1
+                serial_number_list[2:5] = [0, 0, 0]
+                serial_number = generate_serial_number(serial_number_list, 2, args.m)
+                temp_line = temp_line.replace("### ", "### " + str(serial_number), 1)
+                output_file.write(temp_line)
+            elif re.match("## ", temp_line) is not None:
+                serial_number_list[0] += 1
+                serial_number_list[1:5] = [0, 0, 0, 0]
+                serial_number = generate_serial_number(serial_number_list, 1, args.m)
+                temp_line = temp_line.replace("## ", "## " + str(serial_number), 1)
+                output_file.write(temp_line)
+            else:
+                output_file.write(temp_line)
+            output_file.flush()
+            temp_line = input_file.readline()
+            counter += 1
+    output_file.close()
+    print("serial number added")
 
 
 if __name__ == '__main__':
     args = get_common_args()
-    print(args)
-    serial_number_list = [1] * 6
-    print(serial_number_list)
-    print(generate_serial_number(serial_number_list, args.m))
+    add_serial_number(args)
+
 
